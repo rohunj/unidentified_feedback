0d29df0fbbd24dbb19d52e3b92efe0b8465b6275
Rohunj <rohunjauhar@gmail.com>
2022-11-22 10:32:38 -0600

    modified fdbck model and schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_203545) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "feedbacks", force: :cascade do |t|
-    t.string "text"
-    t.string "recipient_handle"
-    t.string "tweet_url"
-    t.string "tweet_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # create_table "feedbacks", force: :cascade do |t|
+  #   t.string "text"
+  #   t.string "recipient_handle"
+  #   t.string "tweet_url"
+  #   t.string "tweet_id"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
 
 end
