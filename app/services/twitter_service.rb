ef87fb3cb2618b66a21e609d20927e0b6cd0b23a
Rohunj <rohunjauhar@gmail.com>
2022-11-24 12:08:41 -0600

    modified twitter_service to save tweet id and url

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -32,8 +32,16 @@ def self.tweet!(feedback)
     response = req.run
 
     if response.success?
+      tweet_id = JSON.parse(response.body)['data']['id']
+      tweet_url = "https://twitter.com/twitter/status/#{tweet_id}"
+
+      feedback.update(tweet_id: tweet_id, tweet_url: tweet_url)
+
+      #TODO: delete logs
       puts "Credentials work! Tweet:"
       puts JSON.pretty_generate(JSON.parse(response.body))
+
+
     else
       puts "Credentials failed, please try generating again."
     end
