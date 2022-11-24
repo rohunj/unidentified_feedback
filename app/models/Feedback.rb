3add35514d2c640c2dfae8991e7646465c47bb0a
Rohunj <rohunjauhar@gmail.com>
2022-11-24 10:21:34 -0600

    added client and server side validations and updated front end character counter

--- app/models/Feedback.rb
+++ app/models/Feedback.rb
@@ -1,7 +1,17 @@
 class Feedback < ApplicationRecord
+  # validations
+  before_create :sanitize
   after_create :send_tweet
 
+  validates_presence_of :recipient_handle, :text
+  validates :text, length: { in: 1 .. 250}
+
+def sanitize
+  #insert ANY logic we want regarding the transformation of text, recipient_handle, etc fields
+  self.recipient_handle = self.recipient_handle.gsub('@', '')
+end
+
   def send_tweet
-    TwitterService.tweet!(text) #1. look for text function, then expect it to be on object itself
+    TwitterService.tweet!(self)
   end
 end
